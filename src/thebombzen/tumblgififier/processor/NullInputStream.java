e4fedd3e48474f080161f36d57ac988a881ef316
Leo Izen <leo.izen@gmail.com>
2016-02-07 11:29:26 -0500

    Made NullInputStream slightly better

--- src/thebombzen/tumblgififier/processor/NullInputStream.java
+++ src/thebombzen/tumblgififier/processor/NullInputStream.java
@@ -10,4 +10,24 @@ public class NullInputStream extends InputStream {
 		return -1;
 	}
 	
+	@Override
+	public int read(byte[] buf, int off, int len){
+		return -1;
+	}
+	
+	@Override
+	public int read(byte[] buf){
+		return -1;
+	}
+	
+	@Override
+	public int available(){
+		return 0;
+	}
+	
+	@Override
+	public void close(){
+		// Do nothing
+	}
+	
 }
