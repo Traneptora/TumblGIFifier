e50412c44468c1d192f79e33b0c3a6c3f8ba8a05
Leo Izen <leo.izen@gmail.com>
2016-06-28 19:57:03 -0400

    Added option to download

--- src/thebombzen/tumblgififier/gui/AboutDialog.java
+++ src/thebombzen/tumblgififier/gui/AboutDialog.java
@@ -1,11 +1,14 @@
 package thebombzen.tumblgififier.gui;
 
 import java.awt.BorderLayout;
+import java.awt.Desktop;
 import java.awt.EventQueue;
 import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -31,7 +34,7 @@ public class AboutDialog extends JDialog {
 		box.add(TumblGIFifier.wrapLeftAligned(new JLabel("Licensed under the MIT license")));
 		box.add(TumblGIFifier.wrapLeftAligned(new JLabel("with included public domain XZ Utils")));
 		box.add(Box.createVerticalStrut(10));
-		box.add(TumblGIFifier.wrapLeftAligned(new JLabel("See https://github.com/thebombzen/TumblGIFifier for details.")));
+		box.add(TumblGIFifier.wrapLeftAligned(new JLabel("See https://thebombzen.github.io/TumblGIFifier/ for details.")));
 		box.add(Box.createVerticalStrut(10));
 		final JButton checkForUpdates = new JButton("Check for updates");
 		final JButton close = new JButton("Close");
@@ -65,9 +68,23 @@ public class AboutDialog extends JDialog {
 							} else {
 								EventQueue.invokeLater(new Runnable(){
 									public void run(){
-										JOptionPane.showMessageDialog(AboutDialog.this, String.format("Update Available!%nLatest Version: %s", latest), "New Version", JOptionPane.INFORMATION_MESSAGE);
+										int answer = JOptionPane.showConfirmDialog(AboutDialog.this, String.format("Update Available!%nLatest Version: %s%nDo you want to download the latest version?", latest), "New Version", JOptionPane.YES_NO_OPTION);
 										close.setEnabled(true);
 										checkForUpdates.setText("Updates Available");
+										if (answer == JOptionPane.YES_OPTION){
+											if (Desktop.isDesktopSupported()){
+												try {
+													Desktop.getDesktop().browse(TumblGIFifier.wrapSafeURL("https://github.com/thebombzen/TumblGIFifier/releases/").toURI());
+												} catch (RuntimeException ex){
+													throw ex;
+												} catch (Exception e){
+													e.printStackTrace();
+													JOptionPane.showMessageDialog(AboutDialog.this, "Error opening web browser.", "Error", JOptionPane.ERROR_MESSAGE);
+												}
+											} else {
+												JOptionPane.showMessageDialog(AboutDialog.this, "Error opening web browser.", "Error", JOptionPane.ERROR_MESSAGE);
+											}
+										}
 									}
 								});
 							}
