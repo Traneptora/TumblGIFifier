cad4d8f1b56ab1eeef5a24079c9917008d43956b
Leo Izen <leo.izen@gmail.com>
2017-10-21 18:21:32 -0400

    organize imports

--- src/thebombzen/tumblgififier/LibraryLoader.java
+++ src/thebombzen/tumblgififier/LibraryLoader.java
@@ -2,7 +2,6 @@ package thebombzen.tumblgififier;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
