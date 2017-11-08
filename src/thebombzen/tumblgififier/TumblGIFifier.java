6a91f646243da0b86045a8c8cf5522fb34a34f25
Leo Izen <leo.izen@gmail.com>
2017-11-08 09:54:42 -0500

    version: Bump to 0.8.0

--- src/thebombzen/tumblgififier/TumblGIFifier.java
+++ src/thebombzen/tumblgififier/TumblGIFifier.java
@@ -21,7 +21,7 @@ import thebombzen.tumblgififier.util.text.StatusProcessor;
 public final class TumblGIFifier {
 
 	/** The version of TumblGIFifier */
-	public static final String VERSION = "0.7.4";
+	public static final String VERSION = "0.8.0";
 
 	/**
 	 * This is so we can cleanup a mess left by older versions. The version
